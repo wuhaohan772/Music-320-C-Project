dc163c866b8e7174b3a3ba2e93d28896c02be031
wuhaohan772 <hwu27@stanford.edu>
2025-06-08 14:55:58 -0700

    initial commits

---
+++ JuceLibraryCode/BinaryData.cpp
@@ -0,0 +1,173 @@
+/* ==================================== JUCER_BINARY_RESOURCE ====================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#include <cstring>
+
+namespace BinaryData
+{
+
+//================== Layout_HW3.xml ==================
+static const unsigned char temp_binary_data_0[] =
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+"\r\n"
+"<magic>\r\n"
+"  <Styles>\r\n"
+"    <Style name=\"default\">\r\n"
+"      <Nodes/>\r\n"
+"      <Classes>\r\n"
+"        <plot-view border=\"2\" background-color=\"black\" border-color=\"silver\" display=\"contents\"/>\r\n"
+"        <nomargin margin=\"0\" padding=\"0\" border=\"0\"/>\r\n"
+"        <group margin=\"5\" padding=\"5\" border=\"2\" flex-direction=\"column\"/>\r\n"
+"      </Classes>\r\n"
+"      <Types>\r\n"
+"        <Slider border=\"0\" slider-textbox=\"textbox-below\"/>\r\n"
+"        <ToggleButton border=\"0\" max-height=\"50\" caption-size=\"0\" text=\"Active\"/>\r\n"
+"        <TextButton border=\"0\" max-height=\"50\" caption-size=\"0\"/>\r\n"
+"        <ComboBox border=\"0\" max-height=\"50\" caption-size=\"0\"/>\r\n"
+"        <Plot border=\"0\" margin=\"0\" padding=\"0\" background-color=\"00000000\"\r\n"
+"              radius=\"0\"/>\r\n"
+"        <XYDragComponent border=\"0\" margin=\"0\" padding=\"0\" background-color=\"00000000\"\r\n"
+"                         radius=\"0\"/>\r\n"
+"      </Types>\r\n"
+"      <Palettes>\r\n"
+"        <default/>\r\n"
+"      </Palettes>\r\n"
+"    </Style>\r\n"
+"  </Styles>\r\n"
+"  <View flex-direction=\"column\" resizable=\"1\" resize-corner=\"1\" background-color=\"FF007185\"\r\n"
+"        tab-color=\"FF007185\" margin=\"0\" padding=\"0\" border=\"0\" id=\"b/g color disabled\">\r\n"
+"    <View flex-direction=\"column\" background-color=\"FF007185\" border=\"2\"\r\n"
+"          border-color=\"FF000000\" margin=\"1\" padding=\"0\" id=\"Extra View for b/g color\">\r\n"
+"      <View flex-direction=\"row\" min-height=\"120\" max-height=\"120\" background-color=\"FF007185\"\r\n"
+"            border-color=\"FF000000\" id=\"TopControls\">\r\n"
+"        <View flex-direction=\"column\" flex-grow=\"5\" padding=\"4\" margin=\"1\" id=\"WaveSelectors\">\r\n"
+"            <ComboBox parameter=\"type\" caption=\"Wave Type: \"\r\n"
+"                    caption-placement=\"centred-left\"\r\n"
+"                    background-color=\"FF234489\" border-color=\"FF00C060\" border=\"1\"\r\n"
+"                    margin=\"1\" padding=\"4\"/>\r\n"
+"            <ComboBox parameter=\"fm_type\" caption=\"FM Wave Type: \"\r\n"
+"                    caption-placement=\"centred-left\"\r\n"
+"                    background-color=\"FF234489\" border-color=\"FF00C060\" border=\"1\"\r\n"
+"                    margin=\"1\" padding=\"4\"/>\r\n"
+"        </View>\r\n"
+"        <MidiLearn margin=\"5\" background-color=\"FF234489\" border=\"1\"\r\n"
+"                   border-color=\"FF00C060\" flex-grow=\"1\"\r\n"
+"                   caption=\"MIDI Learn\"/>\r\n"
+"      </View>\r\n"
+"      <View background-color=\"FF007185\" id=\"Sliders\">\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"Attack\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\"\r\n"
+"              pos-x=\"-1.7301%\" pos-y=\"-6.94444%\" pos-width=\"12.4567%\" pos-height=\"100%\">\r\n"
+"          <Slider parameter=\"attack\" slider-type=\"linear-vertical\" default=\"0.1\"\r\n"
+"                  min-value=\"0\" max-value=\"1\" interval=\"0.01\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"Decay\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"decay\" slider-type=\"linear-vertical\" default=\"0.1\"\r\n"
+"                  min-value=\"0\" max-value=\"1\" interval=\"0.01\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"Sustain\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"sustain\" slider-type=\"linear-vertical\" default=\"0.1\"\r\n"
+"                  min-value=\"0\" max-value=\"1\" interval=\"0.01\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"Release\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"release\" slider-type=\"linear-vertical\" default=\"0.1\"\r\n"
+"                  min-value=\"0\" max-value=\"1\" interval=\"0.01\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"Ampitude\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"amplitude\" slider-type=\"linear-vertical\" default=\"0.1\"\r\n"
+"                  min-value=\"0\" max-value=\"1\" interval=\"0.01\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"FM Enable\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"fm_enable\" slider-type=\"linear-vertical\" min-value=\"0\" max-value=\"1\" interval=\"1\" padding=\"0\" margin=\"2\"\r\n"
+"                        background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                        pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"FM Ratio\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"fm_ratio\" slider-type=\"linear-vertical\" default=\"0.1\"\r\n"
+"                  min-value=\"0.01\" max-value=\"8\" interval=\"0.01\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <View flex-direction=\"column\" padding=\"4\" margin=\"1\" caption=\"FM Depth\"\r\n"
+"              border=\"1\" border-color=\"FF00C060\" background-color=\"FF234489\">\r\n"
+"          <Slider parameter=\"fm_depth\" slider-type=\"linear-vertical\" default=\"3.0\"\r\n"
+"                  min-value=\"0\" max-value=\"10\" interval=\"0.1\" padding=\"0\" margin=\"2\"\r\n"
+"                  background-color=\"FF234489\" pos-x=\"-3.22581%\" pos-y=\"1.5015%\"\r\n"
+"                  pos-width=\"100%\" pos-height=\"90.991%\"/>\r\n"
+"        </View>\r\n"
+"        <Plot flex-grow=\"3\" caption=\"Waveform\" margin=\"1\" padding=\"4\" border=\"1\"\r\n"
+"              background-color=\"FF234489\" plot-color=\"FFF3E244\" source=\"oscilloscope\"\r\n"
+"              border-color=\"FF00C060\"/>\r\n"
+"      </View>\r\n"
+"      <View min-height=\"\" background-color=\"FF007185\" display=\"flexbox\" border-color=\"FF000000\"\r\n"
+"            flex-grow=\"1.0\" id=\"Keyboard\">\r\n"
+"        <KeyboardComponent background-color=\"FF007185\" pos-x=\"-0.0672495%\" pos-y=\"53.3531%\"\r\n"
+"                           pos-width=\"100%\" pos-height=\"46.5483%\" padding=\"0\" margin=\"0\"\r\n"
+"                           border=\"0\" border-color=\"FF000000\"/>\r\n"
+"      </View>\r\n"
+"    </View>\r\n"
+"  </View>\r\n"
+"</magic>\r\n";
+
+const char* Layout_HW3_xml = (const char*) temp_binary_data_0;
+
+
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
+{
+    unsigned int hash = 0;
+
+    if (resourceNameUTF8 != nullptr)
+        while (*resourceNameUTF8 != 0)
+            hash = 31 * hash + (unsigned int) *resourceNameUTF8++;
+
+    switch (hash)
+    {
+        case 0x78f29a67:  numBytes = 6959; return Layout_HW3_xml;
+        default: break;
+    }
+
+    numBytes = 0;
+    return nullptr;
+}
+
+const char* namedResourceList[] =
+{
+    "Layout_HW3_xml"
+};
+
+const char* originalFilenames[] =
+{
+    "Layout_HW3.xml"
+};
+
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
+{
+    for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
+            return originalFilenames[i];
+
+    return nullptr;
+}
+
+}
